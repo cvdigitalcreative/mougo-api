3e25fe214c9fb93f7a0f5ac2bc860d52ba70d071
calvinfm <calvinfadhil51@gmail.com>
2020-08-16 13:16:43 +0700

    CRUD kategori blog

--- src/blog/routesBlog.php
+++ src/blog/routesBlog.php
@@ -30,3 +30,26 @@ $app->post('/blog/{id_blog}', function ($request, $response, $args) {
     $uploadedFiles = $request->getUploadedFiles();
     return $response->withJson(editBlog($this->db, $args['id_blog'], $data['judul_blog'], $data['isi_blog'], $data['kategori_blog'], $data['nama_penulis'], $uploadedFiles, $this->get('settings')['upload_dir_foto_blog']), SERVER_OK);
 })->add($tokenCheck);
+
+// GET KATEGORI BLOG
+$app->get('/blog/kategori/', function ($request, $response) {
+    return $response->withJson(getKategoriBlogAll($this->db), SERVER_OK);
+});
+
+// MEMBUAT KATEGORI BLOG
+$app->post('/blog/kategori/', function ($request, $response) {
+    $data = $request->getParsedBody();
+    return $response->withJson(insertKategoriBlogWeb($this->db, $data['nama_kategori']), SERVER_OK);
+});
+
+// MENGUPDATE KATEGORI BLOG
+$app->put('/blog/kategori/{id_kategori}', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    return $response->withJson(updateKategoriBlogWeb($this->db, $args['id_kategori'], $data['nama_kategori']), SERVER_OK);
+});
+
+// MENGUPDATE KATEGORI BLOG
+$app->delete('/blog/kategori/{id_kategori}', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    return $response->withJson(deleteKategoriBlogWeb($this->db, $args['id_kategori']), SERVER_OK);
+});
