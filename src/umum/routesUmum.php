b856b5fda1ef38b17690c655fe386fbab6d19ee8
calvinfm <calvinfadhil51@gmail.com>
2020-10-15 20:00:17 +0700

    test error

--- src/umum/routesUmum.php
+++ src/umum/routesUmum.php
@@ -150,7 +150,9 @@ $app->get('/customer/trip/orderan/', function ($request, $response) {
     $lat_dest = $request->getQueryParam("lat_destinasi");
     $long_dest = $request->getQueryParam("long_destinasi");
     $token = getenv('GOOGLE_MAPS_API_TOKEN');
+    var_dump($token);
     $response_web = file_get_contents("https://maps.googleapis.com/maps/api/directions/json?origin=$lat,$long&destination=$lat_dest,$long_dest&key=$token");
+    var_dump($response_web);
     $response_web = json_decode($response_web);
     $jarak = ceil(($response_web->routes[0]->legs[0]->distance->value) / 1000);
     $harga = new Umum();
